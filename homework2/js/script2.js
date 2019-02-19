55a42d846981d6a2a369a06a5335a5de100e1197
Pavel Klimov <pavel.klimov@osinit.com>
2019-02-19 14:46:28 +0300

    change 3.3

--- homework2/js/script2.js
+++ homework2/js/script2.js
@@ -51,23 +51,7 @@ else{
 
 alert("Задание 3.3");
 
-var a = +prompt("Введите число ");
-var count = 0;
-var temp = a;
-var result = 0;
+var a = prompt("Введите число ");
+var result = a.split("").reverse().join("");
 
-
-
-while (temp != 0){
-    temp = parseInt(temp/10);
-    count++;
-}
-
-var i = count;
-for (var j = 1; j < count; j++){
-    
-    result += parseInt(a % Math.pow(10,j) / Math.pow(10, j - 1)) * Math.pow(10, i-1);
-    i--; 
-}
-result += parseInt(a/Math.pow(10, count - 1));
 alert(result);
